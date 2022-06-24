e09b5d0f0a006ceabc60e2175d251f0ba2f146dd
naman05002 <f20200103@goa.bits-pilani.ac.in>
2022-06-24 15:55:34 +0530

    New site initiallised

--- index.js
+++ index.js
@@ -1,65 +1,91 @@
-var map;
-var Marker1;
+/*Map Initialization*/
+var map = new MapmyIndia.Map('map', {center: [28.6304, 77.2177], zoom: 10, search: false});
+          
 /*Search plugin initialization*/
-var placeOptions={
-    location:[15.392892, 73.880329]/*,
-    geolocation:true,
-    pod:'City',
-    bridge:true,
-    tokenizeAddress:true,*
-    filter:'cop:9QGXAM',
-    hyperLocal:true, Default is false. Location parameter is mandatory to use this parameter.
-    distance:true,
-    width:300,
-    height:300,
-    clearButton:false, //to hide cross button, which is right side of search input
-    blank_callback:function(){console.log("called when click on cross button or input value become blank");}
-    */
-};
-
-    function initMap1() {
-        map = new mappls.Map('map', {
-            center:[15.392892, 73.880329],
-            zoomControl: true,
-            location: true
-        });
-        // console.log(map);
-
-        Marker1 = new mappls.Marker({
-            map: map,
-            position: {
-                "lat": 15.392892, 
-                "lng": 73.880329
-            },
-            fitbounds: true,
-            icon_url: 'https://apis.mapmyindia.com/map_v3/1.png'
-        });
-        map.addListener('load', function () {
-            var window=new mappls.InfoWindow({
-                map:map,
-                content:`<div id = "InfoWindow">BITS Pilani, K.K. Birla Goa Campus</div>`,
-                position:{
-                    lat: 15.392892, 
-                    lng: 73.880329
-                },
-                closeButton: false
-            });
-        });
-        new MapmyIndia.search(document.getElementById("auto"),placeOptions,callback);
-        
-    }
-    function callback(data) { 
-        if(data)
-        {
-            console.log(data);
-            var dt=data[0];
-            if(!dt) return false;
-            var eloc=dt.eLoc;
-            var place=dt.placeName+", "+dt.placeAddress;
-            /*Use elocMarker Plugin to add marker*/
-            if(Marker1) Marker1.remove();
-            Marker1=new MapmyIndia.elocMarker({map:map,eloc:eloc,popupHtml:place,popupOptions:{openPopup:true}}).fitbounds();
-        }
-      }   
-    // var marker;
+  var optional_config={
+      location:[28.61, 77.23],
+     /* pod:'City',
+      bridge:true,
+      tokenizeAddress:true,*
+      filter:'cop:9QGXAM',
+      distance:true,
+      width:300,
+      height:300*/
+  };
+  var LOC1, LOC2;
+  
+  new MapmyIndia.search(document.getElementById("auto"),optional_config,callback);
+  new MapmyIndia.search(document.getElementById("auto2"),optional_config, (data) => {
+    console.log(`LOC2 = ${data[0].placeName}`);console.log(data);LOC2=data[0].placeName;
+  });
+  function foo(){
+    console.log('Loading...')
+    fetch(`https://delhimetroapi.herokuapp.com/metroapi/from=${LOC1.replace(/ /g, '%20')}&to=${LOC2.replace(/ /g, '%20')}`).then(response => {
+      if(!response.ok)throw new Error(`Error:  ${response.status}`);
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+      alert(JSON.stringify(data));
+    })
+    .catch(error => console.error(error))
     
+  }
+ 
+  /*CALL for fix text - LIKE THIS
+   * 
+   new MapmyIndia.search("agra",optional_config,callback);
+   * 
+   * */
+
+  // function pick(){
+  //   var options={
+  //       map: map,
+  //       callback: callback_method
+  //     }
+  //     var picker = new MapmyIndia.placePicker(options);
+  //     function callback_method(data){
+  //       console.log(data);
+  //       alert(JSON.stringify(data));
+  //     }
+  // }
+  var optio = {
+    divId:'nearby_search',
+    map:map,
+    keywords:'Metro Stations',
+    refLocation:[28.632735,77.219696],
+    fitbounds:true,
+    click_callback:function(d){alert(d);}
+  }
+  var nr=MapmyIndia.nearby(optio);
+
+  var x= false;
+  function near(){
+    // document.getElementById("pickerButton").classList[2] = true;
+    // if(document.getElementById("pickerButton").classList[2]==true){
+      if(x==false){
+        MapmyIndia.direction({map:map,start:"28.545,77.545",end:{label:'India Gate, Delhi',geoposition:"1T182A"},divWidth:0},(data)=>console.log(data));
+        x=true;
+      }
+      
+    }
+    
+  
+  
+  document.getElementById("pickerButton").addEventListener("click",foo);
+  var marker;
+  function callback(data) { 
+      if(data)
+      {
+          LOC1=data[0].placeName;
+          console.log(`LOC1 = ${LOC1}`)
+          console.log(data);
+          var dt=data[0];
+          if(!dt) return false;
+          var eloc=dt.eLoc;
+          var place=dt.placeName+", "+dt.placeAddress;
+          /*Use elocMarker Plugin to add marker*/
+          if(marker) marker.remove();
+          marker=new MapmyIndia.elocMarker({map:map,eloc:eloc,popupHtml:place,popupOptions:{openPopup:true}}).fitbounds();
+      }
+    } 
